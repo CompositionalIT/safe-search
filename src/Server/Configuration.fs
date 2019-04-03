1fae26f2fcdfc6530f4845ca9e2894b0b2c52acd
Prashant Pathak <theprash@gmail.com>
2019-04-03 16:16:04 +0100

    Add initial Azure AD auth (visit /api/login to log in)

--- src/Server/Configuration.fs
+++ src/Server/Configuration.fs
@@ -5,4 +5,7 @@ type ConnectionString = ConnectionString of string
 type Configuration =
     { AzureStorage : ConnectionString
       AzureSearch : ConnectionString * string
-      GoogleMapsApiKey : string }
+      GoogleMapsApiKey : string
+      AzureAdDomain : string
+      AzureAdClientId : string
+      AzureAdClientSecret : string }
--- src/Server/Server.fs
+++ src/Server/Server.fs
@@ -4,6 +4,14 @@ open Microsoft.Extensions.Configuration
 open Microsoft.Extensions.DependencyInjection
 open Saturn
 open System.IO
+open FSharp.Control.Tasks
+open Microsoft.AspNetCore.Authentication.Cookies
+open Microsoft.AspNetCore.Http
+open Microsoft.AspNetCore.Authentication.OpenIdConnect
+open System
+open System.Threading.Tasks
+open Microsoft.AspNetCore.Builder
+open Giraffe
 
 let tryGetEnv =
     System.Environment.GetEnvironmentVariable
@@ -19,7 +27,8 @@ let publicPath =
 let webApp searcher appConfig = router {
     forward "/api/search/" (Routers.Search.createRouter searcher appConfig (SafeSearch.Storage.tryGetGeo appConfig.AzureStorage))
     forward "/api/transactions/" (Routers.Transactions.createRouter searcher appConfig.AzureStorage)
-    forward "/api/postcodes/" (Routers.Postcodes.createRouter appConfig.AzureStorage) }
+    forward "/api/postcodes/" (Routers.Postcodes.createRouter appConfig.AzureStorage)
+    get "/api/login" (Auth.requiresAuthentication (challenge "AzureAD") >=> text "Logged in.") }
 
 let configureAppInsights (services : IServiceCollection) =
     match tryGetEnv "APPINSIGHTS_INSTRUMENTATIONKEY" with
@@ -37,11 +46,46 @@ let appConfig =
             .Build()
     { AzureStorage = ConnectionString(builder.GetConnectionString "AzureStorage")
       GoogleMapsApiKey = builder.GetConnectionString "GoogleMapsApiKey"
-      AzureSearch = ConnectionString(builder.GetConnectionString "AzureSearch"), builder.["AzureSearchName"] }
+      AzureSearch = ConnectionString(builder.GetConnectionString "AzureSearch"), builder.["AzureSearchName"]
+      AzureAdDomain = builder.GetConnectionString "AzureAdDomain"
+      AzureAdClientId = builder.GetConnectionString "AzureAdClientId"
+      AzureAdClientSecret = builder.GetConnectionString "AzureAdClientSecret" }
 
 let searcher =
     Search.Azure.Management.initialize Search.Azure.Management.OnlyIfNonExistant appConfig.AzureSearch appConfig.AzureStorage
     Search.Azure.searcher appConfig.AzureSearch appConfig.AzureStorage
+
+let configureOpenIdConnect appConfig (services : IServiceCollection) =
+    services
+        .AddAuthentication(fun options ->
+            options.DefaultAuthenticateScheme <- CookieAuthenticationDefaults.AuthenticationScheme
+            options.DefaultSignInScheme <- CookieAuthenticationDefaults.AuthenticationScheme
+            options.DefaultChallengeScheme <- CookieAuthenticationDefaults.AuthenticationScheme)
+        .AddCookie()
+        .AddOpenIdConnect("AzureAD", fun options -> 
+            options.Authority <- sprintf "https://%s" appConfig.AzureAdDomain
+
+            options.ClientId <- appConfig.AzureAdClientId
+            options.ClientSecret <- appConfig.AzureAdClientSecret
+
+            options.ResponseType <- "code"
+
+            options.Scope.Clear()
+            options.Scope.Add("openid")
+
+            options.CallbackPath <- PathString("/api/callback")
+
+            options.ClaimsIssuer <- "AzureAD"
+
+            options.Events <- OpenIdConnectEvents(
+                OnRedirectToIdentityProvider = fun x ->
+                    x.ProtocolMessage.RedirectUri <- "http://localhost:8080/api/callback"
+                    Task.CompletedTask))
+            .Services
+
+let appAuth (app:IApplicationBuilder) =
+    app
+        .UseAuthentication()
 
 let app =
     application {
@@ -51,6 +95,8 @@ let app =
         use_static publicPath
         use_json_serializer (Thoth.Json.Giraffe.ThothSerializer())
         service_config configureAppInsights
+        service_config (configureOpenIdConnect appConfig)
+        app_config appAuth
         use_gzip
     }
 
